52d932858b8fd5ba6710866c665d83b36d722987
jinhuizxc <1004260403@qq.com>
2017-05-26 15:53:52 +0800

    学习Kotlin第一天

--- src/my/demo/Hello.kt
+++ src/my/demo/Hello.kt
@@ -15,7 +15,12 @@ package my.demo
  *  4、使用字符串模板
  *  5、使用条件表达式
  *  6、使用可空变量以及空值检查(当空值可能出现时应该明确指出该引用可空。)
- *  7、使用值检查并自动转换
+ *  7、使用值检查并自动转换(
+ *  使用 is 操作符检查一个表达式是否是某个类型的实例。
+ *  如果对不可变的局部变量或属性进行过了类型检查，就没有必要明确转换)
+ *  8、使用循环
+ *  9、使用ranges
+ *  10、使用集合
  */
 fun main(args: Array<String>) {
     print("sum of 3 and 5 is ")
@@ -48,6 +53,83 @@ fun main(args: Array<String>) {
     printProduct1("6", "7")
     printProduct1("a", "7")
     printProduct1("6", "b")
+
+    fun printLength(obj: Any) {
+        println("'$obj' string length is " +
+                "${getStringLength(obj) ?: "... err, not a string"} ")
+    }
+    printLength("Incomprehensibilities")
+    printLength(1000)
+    printLength(listOf(Any()))
+
+    // 使用循环 && 对一个集合进行迭代：
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items){
+        println(item)
+    }
+    // 获得循环下标
+    val items2 = listOf("1", "2", "3")
+    for (index in items2.indices){
+        println("item at $index is ${items2[index]}")
+    }
+    // 使用 while 循环
+    val items3 = listOf("apple", "banana", "kiwi")
+    var index = 0
+    while (index < items3.size) {
+        println("item at $index is ${items3[index]}")
+        index++
+    }
+    // 使用 when 表达式
+    println(describe(1))
+    println(describe("Hello"))
+    println(describe(1000L))
+    println(describe(2))
+    println(describe("other"))
+    // 使用ranges
+    // 使用 in 操作符检查数值是否在某个范围内：
+    val x1 = 11
+    val y1 = 9
+    if(x1 in 1..y1 + 1){
+        println("fits in range")
+    }else{
+        println("fits not in range")
+    }
+    // 检查数值是否在范围外：
+    val list = listOf("a", "b", "c")
+    if (-1 !in 0..list.lastIndex) {
+        println("-1 is out of range")
+    }
+    if (list.size !in list.indices) {
+        println("list.indices =" + list.indices)
+        println("list size is out of valid list indices range too")
+    }
+    // 在范围内迭代：
+    for (x in 1..5){
+        print(x)
+    }
+    println()
+    // 或者使用步进：
+    for (x in 1..10 step 2){
+        print(x)
+    }
+    println()
+    for (x in 9 downTo 0 step 3) {
+        print(x)
+    }
+    println()
+    // 使用集合
+    // 使用 in 操作符检查集合中是否包含某个对象
+    val items4 = setOf("apple", "banana", "kiwi")
+    when {
+        "orange" in items4 -> print("juicy")
+        "apple" in items4 -> println("apple is fine too")
+    }
+    // 使用lambda表达式过滤和映射集合：
+    val fruits = listOf("banana", "avocado", "apple", "kiwi")
+    fruits.filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
 }
 
 
@@ -105,9 +187,29 @@ fun printProduct1(arg1: String, arg2: String) {
         println("Wrong number format in arg2: '${arg2}'")
         return
     }
-
     // x 和 y 将会在空值检测后自动转换为非空值
     println(x * y)
 }
+
+fun getStringLength(obj: Any): Int? {
+    if (obj is String) {
+        // obj 将会在这个分支中自动转换为 String 类型
+        return obj.length
+    }
+    // obj 在种类检查外仍然是 Any 类型
+    return null
+}
+// 使用 when 表达式
+fun describe(obj: Any): String =
+        when (obj) {
+             1          -> "One"
+            "Hello"    -> "Greeting"
+            is Long    -> "Long"
+            !is String -> "Not a string"
+            else       -> "Unknown"
+        }
+
+
+
 
 
